9d2bfbdd144a1a328866b5117f8767d9c5de124e
Carlo Fontanos <carl.fontanos@gmail.com>
2016-09-21 16:45:17 +0800

    Changed the title.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Simple Chat Application' });
+  res.render('index', { title:  'Node.js - Simple Chat App' });
 });
 
 module.exports = router;
